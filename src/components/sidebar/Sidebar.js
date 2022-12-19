8f34d15204e19ab2cbf0fc081fb045d4a293aea8
unreact-hatano <mizutama.hahaha@gmail.com>
2022-12-19 13:16:37 +0900

    material ui の導入・表示

---
+++ src/components/sidebar/Sidebar.js
@@ -0,0 +1,16 @@
+import React from "react";
+import TwitterIcon from "@mui/icons-material/Twitter";
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      {/* twitter icon */}
+      <TwitterIcon className="sidebar_twitterIcon" />
+
+      {/* tweet option */}
+      {/* tweet button */}
+    </div>
+  );
+};
+
+export default Sidebar;
