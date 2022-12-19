d0a3aeacd918301d2b98259c234100ac64511ec3
unreact-hatano <mizutama.hahaha@gmail.com>
2022-12-19 13:42:35 +0900

    material ui で iconの実装 SidebarOptionコンポーネントの実装

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,5 +1,15 @@
 import React from "react";
 import TwitterIcon from "@mui/icons-material/Twitter";
+import HomeIcon from "@mui/icons-material/Home";
+import SearchIcon from "@mui/icons-material/Search";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import MailOutlineIcon from "@mui/icons-material/MailOutline";
+import BookmarkBorderIcon from "@mui/icons-material/BookmarkBorder";
+import ListAltIcon from "@mui/icons-material/ListAlt";
+import PermIdentityIcon from "@mui/icons-material/PermIdentity";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
+import SidebarOption from "./SidebarOption";
+import { Button } from "@mui/material";
 
 const Sidebar = () => {
   return (
@@ -8,7 +18,19 @@ const Sidebar = () => {
       <TwitterIcon className="sidebar_twitterIcon" />
 
       {/* tweet option */}
+      <SidebarOption text="ホーム" Icon={HomeIcon} />
+      <SidebarOption text="話題を検索" Icon={SearchIcon} />
+      <SidebarOption text="通知" Icon={NotificationsNoneIcon} />
+      <SidebarOption text="メッセージ" Icon={MailOutlineIcon} />
+      <SidebarOption text="ブックマーク" Icon={BookmarkBorderIcon} />
+      <SidebarOption text="リスト" Icon={ListAltIcon} />
+      <SidebarOption text="プロフィール" Icon={PermIdentityIcon} />
+      <SidebarOption text="もっと見る" Icon={MoreHorizIcon} />
+
       {/* tweet button */}
+      <Button variant="outlined" className="sidebar_tweet">
+        ツイートする
+      </Button>
     </div>
   );
 };
