62222a533c1674534347b3b2aa217cb5c86bd867
unreact-hatano <mizutama.hahaha@gmail.com>
2022-12-23 15:30:49 +0900

    firebaseにデータ追加する挙動を実装 ボタン押下時の打ち込んだ文字列消去を実装

--- src/components/timeline/TweetBox.js
+++ src/components/timeline/TweetBox.js
@@ -1,21 +1,55 @@
 import { Avatar, Button } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
 import "./TweetBox.css";
+import { collection, addDoc } from "firebase/firestore";
+import db from "../../firebase";
 
 const TweetBox = () => {
+  const [tweetMessage, setTweetMessage] = useState("");
+  const [tweetImage, setTweetImage] = useState("");
+
+  const sendTweet = (e) => {
+    // firebaseのデータベースにデータを追加する
+    e.preventDefault();
+
+    addDoc(collection(db, "posts"), {
+      displayName: "よた",
+      userName: "katakoto_777",
+      verified: true,
+      text: tweetMessage,
+      avatar:
+        "https://i.pinimg.com/474x/c4/69/29/c4692959d15395c795a65d473b794509--bobs-funny-minion.jpg",
+      image: tweetImage,
+    });
+
+    setTweetMessage("");
+    setTweetImage("");
+  };
+
   return (
     <div className="tweetBox">
       <form>
         <div className="tweetBox_input">
           <Avatar />
-          <input placeholder="いまどうしてる？" type="text"></input>
+          <input
+            value={tweetMessage}
+            placeholder="いまどうしてる？"
+            type="text"
+            onChange={(e) => setTweetMessage(e.target.value)}
+          ></input>
         </div>
         <input
+          value={tweetImage}
           className="tweetBox_imageInput"
           placeholder="画像のURLを入力してください"
           type="text"
+          onChange={(e) => setTweetImage(e.target.value)}
         ></input>
-        <Button className="tweetBox_tweetButton" type="submit">
+        <Button
+          className="tweetBox_tweetButton"
+          type="submit"
+          onClick={sendTweet}
+        >
           ツイートする
         </Button>
       </form>
